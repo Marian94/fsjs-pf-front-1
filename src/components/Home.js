0650b0c11e8b22e3558a03b65f9cf0f3e312276c
enorma <enorma29@gmail.com>
2020-04-21 21:13:50 -0500

    upload ejemplo

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Home extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    render() {
+        return(
+            <h1>HOLAMUNDO</h1>
+        );
+    }
+}
+
+export default Home;
